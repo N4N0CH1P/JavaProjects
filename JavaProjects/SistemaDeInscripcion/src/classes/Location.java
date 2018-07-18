76366f82d6b3ffbf94cb97c653130c9bfed0a1cc
N4N0CH1P <mariano.hmc@hotmail.com>
2018-07-18 11:39:46 -0500

    Edited the Location.java file

--- JavaProjects/SistemaDeInscripcion/src/classes/Location.java
+++ JavaProjects/SistemaDeInscripcion/src/classes/Location.java
@@ -1,5 +1,61 @@
 package classes;
 
 public class Location {
+	private String country, state, city;
+	private int postalCode;
+	
+	public Location() {
+		country = "";
+		state = "";
+		city = "";
+		postalCode = 0;
+	}
+	
+	public Location(String country, String state, String city) {
+		this.country = country;
+		this.state = state;
+		this.city = city;
+		postalCode = 0;
+	}
+	
+	public Location(String country, String state, String city, int postalCode) {
+		this.country = country;
+		this.state = state;
+		this.city = city;
+		this.postalCode = postalCode;
+	}
 
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(int postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	
 }
